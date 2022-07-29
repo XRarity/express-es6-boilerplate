4432fde77250d77d97f490aff863c57d459cbd91
LaurinJS <egger.laurin@gmail.com>
2022-07-29 19:31:58 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// app.js
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index';
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', indexRouter);
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
+export default app;
